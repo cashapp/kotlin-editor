4f8f5ef11aa8d4ec24fadb7a7b10f340eb3b6333
Tony Robalik <trobalik@squareup.com>
2024-07-08 09:58:12 -0700

    chore: add publish.yml for manual publications.

--- build-logic/conventions/src/main/kotlin/com/squareup/gradle/BasePlugin.kt
+++ build-logic/conventions/src/main/kotlin/com/squareup/gradle/BasePlugin.kt
@@ -77,5 +77,9 @@ internal class BasePlugin(private val project: Project) {
         }
       }
     }
+
+    tasks.named { it == "publishToMavenCentral" }.configureEach { t ->
+      t.notCompatibleWithConfigurationCache("Publishing is incompatible")
+    }
   }
 }
