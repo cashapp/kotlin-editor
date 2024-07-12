66e81091887a8403060b16d25a501fc98e53a635
yissachar <yissachar@squareup.com>
2024-07-12 12:59:15 -0400

    Add trailing newline tests

--- core/src/test/kotlin/cash/grammar/kotlindsl/utils/WhitespaceTest.kt
+++ core/src/test/kotlin/cash/grammar/kotlindsl/utils/WhitespaceTest.kt
@@ -4,6 +4,7 @@ import cash.grammar.kotlindsl.parse.Parser
 import cash.grammar.kotlindsl.utils.Blocks.isBuildscript
 import cash.grammar.kotlindsl.utils.Blocks.isSubprojects
 import cash.grammar.kotlindsl.utils.test.TestErrorListener
+import com.squareup.cash.grammar.KotlinParser
 import com.squareup.cash.grammar.KotlinParser.NamedBlockContext
 import com.squareup.cash.grammar.KotlinParserBaseListener
 import org.antlr.v4.runtime.CommonTokenStream
@@ -11,6 +12,7 @@ import org.antlr.v4.runtime.Token
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.tuple
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
 
 internal class WhitespaceTest {
 
@@ -74,12 +76,68 @@ internal class WhitespaceTest {
       .allSatisfy { assertThat(it).isEqualTo(tuple(" ")) }
   }
 
+  @Test fun `gets trailing newlines for buildscript`() {
+    val buildScript =
+      """
+        plugins {
+          id("kotlin")
+        }
+        
+        subprojects {
+          buildscript {
+            repositories {}
+          }
+        }
+        
+      """.trimIndent()
+
+    val scriptListener = Parser(
+      file = buildScript,
+      errorListener = TestErrorListener {
+        throw RuntimeException("Syntax error: ${it?.message}", it)
+      },
+      listenerFactory = { _, tokens, _ -> TestListener(tokens) }
+    ).listener()
+
+    assertThat(scriptListener.trailingBuildscriptNewlines).isEqualTo(1)
+  }
+
+  @Test fun `gets trailing newlines for kotlin file`() {
+    val buildScript =
+      """
+        class Foo {
+        }
+        
+      """.trimIndent()
+
+    val scriptListener = Parser(
+      file = buildScript.byteInputStream(),
+      errorListener = TestErrorListener {
+        throw RuntimeException("Syntax error: ${it?.message}", it)
+      },
+      startRule = { parser -> parser.kotlinFile() },
+      listenerFactory = { _, tokens, _ -> TestListener(tokens) }
+    ).listener()
+
+    assertThat(scriptListener.trailingKotlinFileNewlines).isEqualTo(1)
+  }
+
   private class TestListener(
     private val tokens: CommonTokenStream
   ) : KotlinParserBaseListener() {
 
     var newlines: List<Token>? = null
     var whitespace: List<Token>? = null
+    var trailingBuildscriptNewlines = 0
+    var trailingKotlinFileNewlines = 0
+
+    override fun enterScript(ctx: KotlinParser.ScriptContext) {
+      trailingBuildscriptNewlines = Whitespace.countTerminalNewlines(ctx, tokens)
+    }
+
+    override fun enterKotlinFile(ctx: KotlinParser.KotlinFileContext) {
+      trailingKotlinFileNewlines = Whitespace.countTerminalNewlines(ctx, tokens)
+    }
 
     override fun exitNamedBlock(ctx: NamedBlockContext) {
       if (ctx.isSubprojects) {
